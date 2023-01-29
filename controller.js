339af0c322fd74aa815bbd5cf22107623ac8bea3
Alex Keener <alexkeener318@gmail.com>
2023-01-28 18:32:45 -0600

    Changes calculator and font

--- controller.js
+++ controller.js
@@ -3,13 +3,7 @@ childWindow = document.getElementById("UI_box")
 window.addEventListener('message', message => {
     document.getElementById('test').innerHTML = message.data["poke"];
 });
-// const childWindow = document.getElementById('frame').contentWindow;
-// window.addEventListener('message', message => {
-//     if (message.source !== childWindow) {
-//         return; 
-//     }
-//     document.getElementById('test').innerHTML = "pizza pie";
-// });
+
 const progressBar = document.getElementById('progressed');
 console.log(progressBar);
 const button = document.getElementById("increase");
@@ -23,3 +17,6 @@ function updateProgress(){
 }
 
 button.addEventListener('click', updateProgress);
+
+
+// puts line through clicked items
